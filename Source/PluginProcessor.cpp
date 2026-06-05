12f62a46c4e8e8377e76f19ddc4f6daff8d7dc64
choi3171 <naanaachicken@gmail.com>
2026-06-05 21:43:30 +0900

    fix: Implement state saving

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -159,15 +159,26 @@ juce::AudioProcessorEditor* PhaseDelayAudioProcessor::createEditor()
 //==============================================================================
 void PhaseDelayAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    auto state = apvts.copyState();
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    if (xml != nullptr)
+        copyXmlToBinary (*xml, destData);
 }
 
 void PhaseDelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary (data, sizeInBytes));
+
+    if (xmlState == nullptr)
+        return;
+
+    if (! xmlState->hasTagName (apvts.state.getType()))
+        return;
+
+    auto newState = juce::ValueTree::fromXml (*xmlState);
+
+    if (newState.isValid())
+        apvts.replaceState (newState);
 }
 
 //==============================================================================
